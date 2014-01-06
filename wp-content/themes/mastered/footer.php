7e4d27c2924d3a0cdac92f9ce80028564c026d2a
Jack Smith <jack@designedbyjack.com>
2014-01-06 18:07:19 +0000

    Added Disqus plugin, changed theme thumbnail

--- wp-content/themes/mastered/footer.php
+++ wp-content/themes/mastered/footer.php
@@ -2,11 +2,11 @@
 			<div class="layout-width blog-footer">
 				<p class='copyright-footer'>&copy; Mastered &mdash; Turning Makers into Masters.</p>
 				<ul class='footer-sitemap'>
-				<li><a href="/">Home</a></li>
-				<li><a href="/pages/purpose">About Us</a></li>
-				<li><a href="/pages/terms-of-service">Terms Of Service</a></li>
-				<li><a href="/pages/faq">How It Works</a></li>
-				<li><a href="/pages/contact">Contact Us</a></li>
+				<li><a href="https://mastered.co/">Home</a></li>
+				<li><a href="https://mastered.co/pages/purpose">About Us</a></li>
+				<li><a href="https://mastered.co/pages/terms-of-service">Terms Of Service</a></li>
+				<li><a href="https://mastered.co/pages/faq">How It Works</a></li>
+				<li><a href="https://mastered.co/pages/contact">Contact Us</a></li>
 				</ul>
 			</div>
 		</footer>
