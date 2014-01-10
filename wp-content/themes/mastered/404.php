3bd0b8fd60b3539e2cbac9c24520fdbc206c2ec9
Jack Smith <jack@designedbyjack.com>
2014-01-10 11:14:15 +0000

    new favicon, 404 page, header clean up

--- wp-content/themes/mastered/404.php
+++ wp-content/themes/mastered/404.php
@@ -1,6 +1,10 @@
 <?php get_header(); ?>
-	<h1>Error 404 - Page Not Found</h1>
-	<div class="entry-content">
-		<p>Oops, this page couldn't be found! Check you didn't mistype the URL, otherwise the page may simply have been deleted.</p>
-	</div> 
+	<div class="layout-width">
+		<div class="col main-content">
+			<h1>Error 404 - Page Not Found</h1>
+			<div class="entry-content">
+				<p>Oops, this page couldn't be found! Check you didn't mistype the URL, otherwise the page may simply have been deleted.</p>
+			</div>
+		</div>
+	</div>
 <?php get_footer(); ?>
