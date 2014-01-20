a748f8cd56f5bf898f9265d4377cc0ba71b2b12f
Jack Smith <jack@designedbyjack.com>
2014-01-20 17:04:42 +0000

    Blog sidebar promo

--- wp-content/themes/mastered/sidebar.php
+++ wp-content/themes/mastered/sidebar.php
@@ -1,3 +1,7 @@
+<a href="https://mastered.co/courses" class="blog-course-promo">
+	<h3>Turning Makers into Masters</h3>
+	<p>Online fashion and jewelry courses from world class experts. Try a course today!</p>
+</a>
 <form role="search" class="blog-search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<label class="search-blog-label screen-reader-text" for="s">Search the blog:</label>
 	<input type="text" value="" class="search-blog-input" name="s" id="s" /><input type="submit" id="searchsubmit" value="Search" class="blog-search-btn" />
