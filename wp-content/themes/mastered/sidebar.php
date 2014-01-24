0ff13294cd705e285dac8a3cdbc215b40a74eed3
Jack Smith <jack@designedbyjack.com>
2014-01-24 13:37:40 +0000

    with echo

--- wp-content/themes/mastered/sidebar.php
+++ wp-content/themes/mastered/sidebar.php
@@ -1,6 +1,6 @@
 <?php if ( is_single() ) : ?>
 	<div class="white-module with-space less-padding">
-		<p class="bold"><span class="written-by">Written by</span> <?php get_the_author_meta('first_name'); ?> <?php get_the_author_meta('last_name'); ?></p>
+		<p class="bold"><span class="written-by">Written by</span> <?php echo get_the_author_meta('first_name'); ?> <?php echo get_the_author_meta('last_name'); ?></p>
 		<p class="no-spacing bio-para">
 			<span class="blog-bio-pic">
 				<?php echo get_wp_user_avatar(get_the_author_meta('ID'), 'thumbnail'); ?>
