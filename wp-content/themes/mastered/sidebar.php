36e1bf4966cc5c96632f563102f7c6a01ededf3a
Tony Daly <tony.daly@me.com>
2014-06-25 16:55:39 +0100

    Remove categories, archive and search.

--- wp-content/themes/mastered/sidebar.php
+++ wp-content/themes/mastered/sidebar.php
@@ -14,15 +14,3 @@
 	<p class="desc">Online fashion and jewellery courses from world class experts.</p>
 	<p class="blog-cta-wrap"><span class="butt mini cta">Try a course today!</span></p>
 </a>
-<form role="search" class="blog-search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
-	<label class="search-blog-label screen-reader-text" for="s">Search the blog:</label>
-	<input type="text" value="" class="search-blog-input" name="s" id="s" /><input type="submit" id="searchsubmit" value="Search" class="blog-search-btn" />
-</form>
-<ul class="sidebar-archives side-list">
-	<li class="sidebar-title">Blog Archives</li>
-	<?php wp_get_archives(); ?>
-</ul>
-<ul class="sidebar-categories side-list">
-	<li class="sidebar-title">Blog Categories</li>
-	<?php wp_list_categories('orderby=name&title_li='); ?>
-</ul>
