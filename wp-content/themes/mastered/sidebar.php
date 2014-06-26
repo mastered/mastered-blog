efa71e81f966a74e1d813f638c1d9f0f474a90c7
Tony Daly <tony.daly@me.com>
2014-06-26 10:52:44 +0100

    Update sidebar link.

--- wp-content/themes/mastered/sidebar.php
+++ wp-content/themes/mastered/sidebar.php
@@ -9,7 +9,7 @@
 		</p>
 	</div>
 <?php endif; ?>
-<a href="https://mastered.co/courses" class="blog-course-promo">
+<a href="ghttp://www.mastered.com/course-listings" class="blog-course-promo">
 	<h3>Turning Makers into Masters</h3>
 	<p class="desc">Online fashion and jewellery courses from world class experts.</p>
 	<p class="blog-cta-wrap"><span class="butt mini cta">Try a course today!</span></p>
