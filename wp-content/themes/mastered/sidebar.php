50a06db810dcf5942b0d067d576e5bef3752ca59
Tony Daly <tony.daly@me.com>
2014-07-11 15:45:22 +0100

    Remove written by section of sidebar.

--- wp-content/themes/mastered/sidebar.php
+++ wp-content/themes/mastered/sidebar.php
@@ -1,14 +1,3 @@
-<?php if ( is_single() ) : ?>
-	<div class="white-module with-space less-padding">
-		<p class="bold"><span class="written-by">Written by</span> <?php echo get_the_author_meta('first_name'); ?> <?php echo get_the_author_meta('last_name'); ?></p>
-		<p class="no-spacing bio-para">
-			<span class="blog-bio-pic">
-				<?php echo get_wp_user_avatar(get_the_author_meta('ID'), 'thumbnail'); ?>
-			</span>
-			<?php echo get_the_author_meta('description'); ?>
-		</p>
-	</div>
-<?php endif; ?>
 <a href="http://www.mastered.com/course-listings" class="blog-course-promo">
 	<h3>Turning Makers into Masters</h3>
 	<p class="desc">Online fashion and jewellery courses from world class experts.</p>
