eaa8d946a96af118e408b5f8e5687b83d3185343
David Kariukí <dkariuki@gmail.com>
2014-08-25 21:00:08 -0700

    Add remarketing code

--- wp-content/themes/mastered/footer.php
+++ wp-content/themes/mastered/footer.php
@@ -228,6 +228,19 @@
   <noscript>
     <a href=olark_contact_url title="Contact us" target="_blank">Questions? Feedback?</a> powered by <a href="http://www.olark.com?welcome" title="Olark live chat software">Olark live chat software</a>
   </noscript>
-
+  <script type="text/javascript">
+  /* <![CDATA[ */
+  var google_conversion_id = 979096793;
+  var google_custom_params = window.google_tag_params;
+  var google_remarketing_only = true;
+  /* ]]> */
+  </script>
+  <script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
+  </script>
+  <noscript>
+  <div style="display:inline;">
+  <img height="1" width="1" style="border-style:none;" alt="" src="//googleads.g.doubleclick.net/pagead/viewthroughconversion/979096793/?value=0&amp;guid=ON&amp;script=0"/>
+  </div>
+  </noscript>
 	</body>
 </html>
