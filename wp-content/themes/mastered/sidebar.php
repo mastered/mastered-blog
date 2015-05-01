ee390d51dbe67146b6618b5a79ec6c92a046f2af
Aleksandar Vucic <vucinjo@gmail.com>
2015-05-01 13:18:02 +0200

    Remove courses from sidebar. Update pinterest link

--- wp-content/themes/mastered/sidebar.php
+++ wp-content/themes/mastered/sidebar.php
@@ -19,44 +19,9 @@
 <h3>Connect with us</h3>
 <div class="share-links">
   <a href="https://twitter.com/masteredhq" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/twitter.png"/></a>
-  <a href="https://www.pinterest.com/Masteredpin/" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/pinterest.png"/></a>
+  <a href="https://www.pinterest.com/masteredhq" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/pinterest.png"/></a>
   <a href="https://www.facebook.com/MasteredHQ" target="_blank"><img src="<?php echo get_template_directory_uri(); ?>/images/facebook.png"/></a>
 </div>
-
-<hr/>
-<h3>Explore courses</h3>
-<ul class="course-listing">
-  <li>
-    <a href="https://www.mastered.com/course-listings/master-surface-embellishment/overview" target="_blank">
-      <div class="course-image"><img src="<?php echo get_template_directory_uri(); ?>/images/embellishment.jpg"/></div>
-      <div class="course-description">
-        <h3 class="bold">Master Surface Embellishment</h3>
-        <div class="description">with Hand & Lock, Diana Springall and Karen Nicoll</div>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="https://www.mastered.com/course-listings/the-savile-row-coat/overview" target="_blank">
-      <div class="course-image"><img src="<?php echo get_template_directory_uri(); ?>/images/savilerow.jpg"/></div>
-      <div class="course-description">
-        <h3 class="bold">The Savile Row Coat</h3>
-        <div class="description">with the Savile Row Academy</div>
-      </div>
-    </a>
-  </li>
-  <li>
-    <a href="https://www.mastered.com/course-listings/develop-your-illustration-career/overview" target="_blank">
-      <div class="course-image"><img src="<?php echo get_template_directory_uri(); ?>/images/illustration.jpg"/></div>
-      <div class="course-description">
-        <h3 class="bold">Develop your illustration career</h3>
-        <div class="description">with Jacqueline Bisset</div>
-      </div>
-    </a>
-  </li>
-</ul>
-
-<a class="butt mini cta full-width" href="https://mastered.com/course-listings">Explore all courses</a>
-
 <hr/>
 <form role="search" class="blog-search" method="get" id="searchform" action="<?php echo home_url( '/' ); ?>">
 	<h3>Search the blog:</h3>
